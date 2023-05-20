398e9d7ede801457cffe4dfff5d47904ede2752e
Ronald George <ronaldsamgeorge@gmail.com>
2023-05-20 18:33:49 +0530

    Add API Gateway infrastructure code

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,42 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# create dynamodb 
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "cloudresume"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "CounterId"
+
+  attribute {
+    name = "CounterId"
+    type = "N"
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
+
+# create The API Gateway API
+resource "aws_api_gateway_rest_api" "getCountApi" {
+  name = "count"
+  description = "API to retrieve visitor count"
+}
+
+#create API resource 
+resource "aws_api_gateway_resource" "getCountResource" {
+    parent_id = aws_api_gateway_rest_api.getCountApi.root_resource_id
+    path_part = "count"
+    rest_api_id = aws_api_gateway_rest_api.getCountApi.id
+}
+
+# create API resource Method
+resource "aws_api_gateway_method" "getCountMethod" {
+  authorization = "NONE"
+  http_method = "GET"
+  resource_id = aws_api_gateway_resource.getCountResource.id
+  rest_api_id = aws_api_gateway_rest_api.getCountApi.id
+}
+
