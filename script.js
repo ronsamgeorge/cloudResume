f4ab4df00317d78cbdccb792a90d44299db8f90e
Ronald George <ronaldsamgeorge@gmail.com>
2023-05-20 12:01:59 +0530

    Initialized terraform file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// get the count from the api gateway endpoint
+// get the count from the backend API
 const getCount = async () => {
   const response = await fetch(
     "https://j1ys53oi0g.execute-api.ap-southeast-2.amazonaws.com/count"
@@ -9,11 +9,10 @@ const getCount = async () => {
   return data;
 };
 
+// set the visitor count
 const setVisitorCount = async () => {
   const count = await getCount();
-
   const countDiv = document.querySelector(".count-value");
-
   countDiv.innerText = await count;
 };
 
